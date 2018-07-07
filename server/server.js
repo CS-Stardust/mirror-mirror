52c7deed1a75da4340937efed29a29c9afe87f26
Joe Thel <jgraythel@gmail.com>
2018-07-06 23:00:58 -0700

    ♻  (server): fix generic route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+
 const app = express();
 
 app.get('/', (req, res) => res.send('we are connected!'));
-app.get('*'), (req, res) => res.status(404).send('error!');
+app.use((req, res) => res.status(404).send('error!'));
 
 app.listen(3000, () => console.log('listening at port 3000...'));
