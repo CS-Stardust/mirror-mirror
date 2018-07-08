d4d76146599f43e17d6412bb2ca3aa5ece0d8814
Joe Thel <jgraythel@gmail.com>
2018-07-08 13:15:04 -0700

    ✨ ➕ (src): add react-router-dom and simple routing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom';
 import './App.css';
-// import InterviewForm from './components/InterviewForm';
+import InterviewForm from './components/InterviewForm';
 import InterviewDetail from './components/InterviewDetail';
-
-const dummyDetail = {
-  displayName: 'Joe Thel',
-  handle: 'fakemonster',
-  position: 'DevOps',
-  company: 'IDEO',
-  date: '2018-07-04',
-  questions: [
-    'Tell us how you got into the world of coding.',
-    'Explain what server-side rendering is and how it\'s important.\nHello',
-    'Find all combinations of square numbers that can add to 120.',
-  ],
-  notes: 'Generally a good experience!\n\nI definitely wish I had done more research into architecture, it would have helped a lot. They seem to be most concerned with how much independence you can display.',
-};
+import dummyDetail from './sampleInterview';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Mirror, Mirror</h1>
-        </header>
-        {/* <InterviewForm /> */}
-        <InterviewDetail {...dummyDetail} />
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Link to="/"><h1 className="App-title">Mirror Mirror</h1></Link>
+            <nav>
+              <Link to="/interviews/new">Add an Interview</Link>
+              <Link to="/interviews/sample">View Sample Interview</Link>
+            </nav>
+          </header>
+          <Switch>
+            <Route exact path="/interviews/new" component={InterviewForm} />
+            <Route
+              path="/interviews/:id"
+              render={props => (
+                <InterviewDetail {...dummyDetail} {...props} />
+              )}
+            />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/sampleInterview.js
@@ -0,0 +1,14 @@
+export default {
+  displayName: 'Joe Thel',
+  handle: 'fakemonster',
+  position: 'DevOps',
+  company: 'IDEO',
+  date: '2018-07-04',
+  questions: [
+    'Tell us how you got into the world of coding.',
+    'Explain what server-side rendering is and how it\'s important.\nHello',
+    'Find all combinations of square numbers that can add to 120.',
+  ],
+  notes: 'Generally a good experience!\n\nI definitely wish I had done more research into architecture, it would have helped a lot. They seem to be most concerned with how much independence you can display.',
+};
+
