dc6e907212d262f7d34cb78587d8f4c4f1c03141
Joe Thel <jgraythel@gmail.com>
2018-07-07 17:38:57 -0700

    🎨 (src/App): extract placeholder data into separate object

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import './App.css';
 // import InterviewForm from './components/InterviewForm';
 import InterviewDetail from './components/InterviewDetail';
 
+const dummyDetail = {
+  displayName: 'Joe Thel',
+  handle: 'fakemonster',
+  position: 'DevOps',
+  company: 'IDEO',
+  date: '2018-07-04',
+  questions: [
+    'Tell us how you got into the world of coding.',
+    'Explain what server-side rendering is and how it\'s important.\nHello',
+    'Find all combinations of square numbers that can add to 120.',
+  ],
+  notes: 'Generally a good experience!\n\nI definitely wish I had done more research into architecture, it would have helped a lot. They seem to be most concerned with how much independence you can display.',
+};
+
 class App extends Component {
   render() {
     return (
@@ -10,20 +24,8 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Mirror, Mirror</h1>
         </header>
-        {/*<InterviewForm />*/}
-        <InterviewDetail
-          displayName="Joe Thel"
-          handle="fakemonster"
-          position="DevOps"
-          company="IDEO"
-          date="2018-07-04"
-          questions={[
-            'Tell us how you got into the world of coding.',
-            'Explain what server-side rendering is and how it\'s important.\nHello',
-            'Find all combinations of square numbers that can add to 120.',
-          ]}
-          notes={'Generally a good experience!\n\nI definitely wish I had done more research into architecture, it would have helped a lot. They seem to be most concerned with how much independence you can display.'}
-        />
+        {/* <InterviewForm /> */}
+        <InterviewDetail {...dummyDetail} />
       </div>
     );
   }
